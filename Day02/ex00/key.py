5f06b7e00f3d2eb44fb5f0b5db994b0246f9b184
Aleksandra Smolniakova <smolnyakovaae@gmail.com>
2024-09-21 23:14:28 +0000

    ex00

--- Day02/ex00/key.py
+++ Day02/ex00/key.py
@@ -3,24 +3,19 @@ class Key:
         self.passphrase = "zax2rulez"
     
     def __len__(self):
-        # This makes len(key) return 1337
         return 1337
     
     def __getitem__(self, index):
-        # This makes key[404] return 3
         if index == 404:
             return 3
         raise IndexError("Key only has the 404th element.")
 
     def __gt__(self, other):
-        # This makes key > 9000 true
-        return other < 9000
+        return other <= 9000
 
     def __str__(self):
-        # This makes str(key) return "GeneralTsoKeycard"
         return "GeneralTsoKeycard"
 
-# Testing the assertions
 if __name__ == "__main__":
     key = Key()
     assert len(key) == 1337, "Length of key is not 1337!"
